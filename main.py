49eef6dd179bf25ee999f32d17870476c7f45583
Yonatan Piasetzky <pitz.1337@gmail.com>
2022-09-12 14:49:50 +0300

    Added photon circuit class and an example usecase

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from photonic_circuit import PhotonicCircuit
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    I = np.eye(2)
+    X = np.array([[0, 1], [1, 0]])
+    X_sqrt = np.cos(np.pi/4) * I - np.sin(np.pi/4) * 1j * X
+    circuit = PhotonicCircuit(4)
+    circuit.apply_coupler(X_sqrt, 0, 1)
+    circuit.apply_coupler(X_sqrt, 2, 3)
+    circuit.apply_coupler(-1j*X, 1, 2)
+    circuit.apply_coupler(X_sqrt, 0, 1)
+    circuit.apply_coupler(X_sqrt, 2, 3)
+    basis = [[0, 2], [0, 3], [1, 2], [1, 3]]
+    U = circuit.project_on_computation_basis(basis)
+    print(U)
---
+++ photonic_circuit.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class PhotonicCircuit:
+    def __init__(self, n):
+        self.n = n
+        self.unitary = np.eye((self.n), dtype=np.complex128)
+    
+    def apply_coupler(self, coupler, wg1, wg2):
+        coup_matrix = np.eye((self.n), dtype=np.complex128)
+        coup_matrix[wg1, wg1] = coupler[0, 0]
+        coup_matrix[wg1, wg2] = coupler[0, 1]
+        coup_matrix[wg2, wg1] = coupler[1, 0]
+        coup_matrix[wg2, wg2] = coupler[1, 1]
+        self.unitary = np.matmul(coup_matrix, self.unitary)
+    
+    def project_on_computation_basis(self, basis):
+        m = len(basis) # 2^Number of qubits
+        logical_unitary = np.zeros((m, m), dtype=np.complex128)
+        for i in range(m):
+            for j in range(m):
+                v_ind = [basis[i][0], basis[i][1]]
+                u_ind = [basis[j][0], basis[j][1]]
+                logical_unitary[i, j] = self.unitary[v_ind[0], u_ind[0]] * self.unitary[v_ind[1], u_ind[1]] + self.unitary[v_ind[0], u_ind[1]] * self.unitary[v_ind[1], u_ind[0]]
+        return logical_unitary
